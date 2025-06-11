0248ca46a9af5087c671f096009fe22be3a330f9
gervasiogabriel <httpgervasio@hotmail.com>
2025-06-11 20:29:32 -0300

    JS02 ExercicioCasa Finalizado

---
+++ JS/JS02-String/JS02-ExercicioCasa/main.js
@@ -0,0 +1,7 @@
+// STRING
+var t1 = "Estamos aprendendo JavaScript e seremos futuros Devs"
+console.log(t1.substring(19, 20));
+console.log(t1.length);
+console.log(t1.toUpperCase());
+console.log(t1.toLowerCase());
+console.log(t1.substring(48, 53));
