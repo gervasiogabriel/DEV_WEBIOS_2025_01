cf74460d6b336d7d256d857cb1463ef086a44557
gervasiogabriel <httpgervasio@hotmail.com>
2025-06-11 20:08:07 -0300

    JS01 ExercicioCasa Finalizado

---
+++ JS/JS01-Introducao/JS01-ExercicioCasa/main.js
@@ -0,0 +1,17 @@
+// NOTAS DE ALUNOS
+// var n1 = 8
+// var n2 = 6
+// var n3 = 9
+// var n4 = 10
+// var m = 4
+// n1 = n1 + n2 + n3 + n4 
+// n1 = n1 / m
+// console.log(n1);
+
+var n1 = 8
+var n2 = 6
+var n3 = 9
+var n4 = 10
+var resultado = 0
+resultado = (n1 + n2 + n3 + n4) / 4 
+console.log(resultado);
