cc889337c5c3934d88361a580b2e5ba9cee112a8
Gabriel Gervásio <gervasiodev@hotmail.com>
2025-06-15 16:10:02 -0300

    JS Aula03 ExercicioCasa

--- JS/JS03-Condicional/JS03-ExercicioCasa/main.js
+++ JS/JS03-Condicional/JS03-ExercicioCasa/main.js
@@ -3,6 +3,7 @@ var numero2 = 9
 
 if (numero1 > numero2){
     console.log("numero1 é maior que numero2")
-} else if ( numero1 < numero2) {
+} 
+else if ( numero1 < numero2) {
    console.log("numero1 é menor que numero2")
 }
