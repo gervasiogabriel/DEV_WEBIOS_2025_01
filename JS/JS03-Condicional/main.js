ec47d7b582624468ad2f173d2fc8c4c99c19a0fd
Gabriel Gervásio <gervasiodev@hotmail.com>
2025-06-15 15:59:48 -0300

    JS Aula03 Finalizada

--- JS/JS03-Condicional/main.js
+++ JS/JS03-Condicional/main.js
@@ -17,3 +17,15 @@
     else{ // RESPOSTA ALTERNATIVA
         console.log("você inseriu uma informação invalida");
     }
+
+    let nota = 6.9
+    // se a nota for igual igual ou maior que 7 então o aluno passou na materia
+    // se nao a nota for menor ou igual a 6.9 o aluno vai ficar de recuperação
+    if(nota >= 7){
+        console.log("Passou na materia")
+    }
+    else{
+        console.log("Vai ficar de recuperação")
+    }
+
+    nota >= 7 ? console.log("Passou na materia") : console.log("Vai ficar de recuperação")
